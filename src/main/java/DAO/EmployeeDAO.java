3e097a89c7620f51caf60f0b2df8c501e413ffbf
GrantDit <kiruha5854@mail.ru>
2023-06-30 02:31:40 +0300

    last commit

---
+++ src/main/java/DAO/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    void create(Employee employee);
+
+    Employee readById(int id);
+
+    List<Employee> readAll();
+
+    void updateEmployeeById(int id, String firstName, String lastName, String gender, int age, int cityId);
+
+    void deleteEmployeeById(int id);
+}
---
+++ src/main/java/main/Application.java
@@ -0,0 +1,52 @@
+package main;
+
+import DAO.EmployeeDAO;
+import DAO.EmployeeDAOImpl;
+import model.City;
+import model.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        // Создаем переменные с данными для подключения к базе
+        final String user = "postgres";
+        final String password = "kirill58";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        // Создаем соединение с базой с помощью Connection
+        // Формируем запрос к базе с помощью PreparedStatement
+
+        try (final Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement("SELECT*FROM employee WHERE city_id = (?)")) {
+            statement.setInt(1, 1);
+            final ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String employeeName = "Name: " + resultSet.getString("first_name");
+                String employeeLastName = "Last name: " + resultSet.getString("last_name");
+                String employeeGender = "Gender: " + resultSet.getString("gender");
+                String employeeCity = "City: " + resultSet.getString("city_name");
+                System.out.println(employeeName);
+                System.out.println(employeeLastName);
+                System.out.println(employeeGender);
+                System.out.println(employeeCity);
+            }
+        }
+        try (final Connection connection = DriverManager.getConnection(url, user, password)) {
+            EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
+
+            employeeDAO.create(new Employee("Petr", "Petrov", "male", 20, new City(2, "Moscow")));
+
+            System.out.println(employeeDAO.readById(4));
+
+            List<Employee> employeeList = new ArrayList<>(employeeDAO.readAll());
+            employeeList.forEach(System.out::println);
+
+            employeeDAO.updateEmployeeById(8, "Ksenya", "Ivanova", "woman", 32, 1);
+
+            employeeDAO.deleteEmployeeById(8);
+        }
+    }
+}
